00960d516ecbbc41874cc3d8ecf3cf2ee441ea04
AnaBaggio <anajulyasouza29@gmail.com>
2023-02-14 11:00:55 -0300

    Meu primeiro programa PHP

---
+++ aula1/ola2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Aqui é o código HTML -->
+    <h1>Exemplo: Olá mundo!</h1>
+    <?php
+        //Aqui é o código PHP
+        echo "<h2>IFSP - Campus Birigui</h2>"
+    ?>
+</body>
+</html>
